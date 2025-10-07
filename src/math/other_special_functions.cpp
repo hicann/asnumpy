3cb0ad1601c97a8403cd444a85aa0a552b566038
Sea--1024 <18229820470@139.com>
2025-10-07 08:23:47 +0800

    debug other_special_functions.cpp/hpp, implement other_special_functions.md
    Co-authored-by: Sea--1024<18229820470@139.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge test2 into master
    
    debug other_special_functions.cpp/hpp, implement other_special_functions.md
    
    Created-by: Sea--1024
    Commit-by: Sea--1024
    Merged-by: ascend-robot
    Description: 类型
    Bug修复
    文档内容更新
    
    ## 描述
    修改了other_special_functions.cpp和other_special_functions.cpp.hpp文件，主要加了namespace和函数添加了dtype参数
    添加了other_special_functions.cpp.md说明文档
    
    ## 如何测试
    测试这个文件的测试代码和数据我还没上传，因为多人修改同一个文件经常会遇到合并冲突，之后会一起上传
    
    ## Checklist
    我已经自己测试过我的代码，更新了相应文档
    
    See merge request: cann/asnumpy!11

--- src/math/other_special_functions.cpp
+++ src/math/other_special_functions.cpp
@@ -5,11 +5,22 @@
 #include <fmt/format.h>
 #include <stdexcept>
 
+namespace asnumpy {
+
 /**
  * @brief Element-wise sinc function using aclnnSinc.
  */
-NPUArray sinc(const NPUArray& x) {
-    auto out = NPUArray(x.shape, x.aclDtype);
+NPUArray Sinc(const NPUArray& x, std::optional<py::dtype> dtype) {
+    py::dtype py_dtype = x.dtype;
+    aclDataType in_dtype = NPUArray::GetACLDataType(py_dtype);
+    aclDataType out_dtype = in_dtype;
+    // 再把 out_dtype 转回 py::dtype，传给 NPUArray 构造函数
+    py::dtype out_py_dtype = NPUArray::GetPyDtype(out_dtype);
+    if (dtype != std::nullopt) {
+        out_py_dtype = *dtype;
+        out_dtype = NPUArray::GetACLDataType(out_py_dtype);
+    }
+    NPUArray out(x.shape, out_py_dtype);
 
     uint64_t workspaceSize = 0;
     aclOpExecutor* executor = nullptr;
@@ -59,4 +70,6 @@ NPUArray sinc(const NPUArray& x) {
     }
 
     return out;
+}
+
 }
