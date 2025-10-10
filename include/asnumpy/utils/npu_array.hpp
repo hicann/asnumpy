0c46381d9524c6cbdd7b7a59f1fea65c36b96b01
gehaoxuan <2310314646@qq.com>
2025-10-10 19:12:53 +0800

    add Apache-2.0 license header to include files
    Co-authored-by: gehaoxuan<2310314646@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    merge license into master
    
    add Apache-2.0 license header to include files
    
    Created-by: gehaoxuan
    Commit-by: gehaoxuan
    Merged-by: ascend-robot
    Description: ## 类型
    - [ ] Bug修复
    - [ ] 新功能
    - [ ] 代码风格更新
    - [ ] 代码重构
    - [ ] 构建过程或辅助工具的变动
    - [ ] 文档内容更新
    
    ## 描述
    请提供此 Pull Request 的详细说明。
    
    ## 如何测试
    描述测试这个变更的步骤，包括哪些文件需要被修改。
    
    ## Checklist:
    - [ ] 我的代码遵循这个项目的代码风格
    - [ ] 我已经自己测试过我的代码
    - [ ] 我已经更新了相应的文档
    - [ ] 我已经根据需要更新了对应的变更日志
    - [ ] 我已经在标题中正确使用了类型标签（例如：`feature:`, `fix:`）
    
    ## 其他信息
    在这里可以添加任何与这个 Pull Request 相关的其他说明。
    
    
    See merge request: cann/asnumpy!42

--- include/asnumpy/utils/npu_array.hpp
+++ include/asnumpy/utils/npu_array.hpp
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright [2024]-[2025] [HIT1920/asnumpy] Authors. All Rights Reserved.
+ * Copyright (c) 2025 AISS Group at Harbin Institute of Technology. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
